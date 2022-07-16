e12900e63be058d0a7f881a9a52046550de94ffa
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-16 14:44:28 +0200

    Connect the UI with ToDoVC

--- ToDoList/ToDoViewController.swift
+++ ToDoList/ToDoViewController.swift
@@ -2,9 +2,17 @@ import UIKit
 
 class ToDoViewController: UIViewController {
 
+    @IBOutlet weak var toDoItemText: UITextField!
+    
+    @IBOutlet weak var prioritySegment: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addToDoItem(_ sender: Any) {
     }
 }
 
