ad2094ab2600a51d1321ac72942e0d79bfc90f08
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-16 17:07:45 +0200

    Change the app transport security settings to allow the arbitrary loads

--- ToDoList/Controller/ToDoViewController.swift
+++ ToDoList/Controller/ToDoViewController.swift
@@ -9,6 +9,7 @@ class ToDoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        NetworkService.shared.getToDo()
     }
 
     @IBAction func addToDoItem(_ sender: Any) {
