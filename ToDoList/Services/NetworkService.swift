19df0b5264e4e427505b273eef435e576119832b
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-16 18:44:53 +0200

    Wrap all the errors and successes in the main thread - so user can see all the data

--- ToDoList/Services/NetworkService.swift
+++ ToDoList/Services/NetworkService.swift
@@ -2,42 +2,43 @@ import Foundation
 
 struct NetworkService {
     static let shared = NetworkService()
-
+    
     let URL_BASE = "http://localhost:3003"
     let URL_ADD_TODO = "/add"
-
+    
     let session = URLSession(configuration: .default)
-
+    
     func getToDos(onSeccess: @escaping (ToDos) -> Void) {
         let url = URL(string: "\(URL_BASE)")!
-
+        
         let task = session.dataTask(with: url) { (data, response , error) in
-            if let error = error {
-                debugPrint(error.localizedDescription)
-                return
-            }
-            guard let data = data, let response = response as? HTTPURLResponse else {
-                debugPrint("Invalid data or response")
-                return
-            }
-            do {
-                if response.statusCode == 200 {
-                    // handle success
-                    let items = try JSONDecoder().decode(ToDos.self, from: data)
-                    onSeccess(items)
-                } else {
-                    // handle error
-                    let err = try JSONDecoder().decode(APIError.self, from: data)
+            DispatchQueue.main.async {
+                if let error = error {
+                    debugPrint(error.localizedDescription)
+                    return
                 }
-            } catch {
-                debugPrint(error.localizedDescription)
+                guard let data = data, let response = response as? HTTPURLResponse else {
+                    debugPrint("Invalid data or response")
+                    return
+                }
+                do {
+                    if response.statusCode == 200 {
+                        // handle success
+                        let items = try JSONDecoder().decode(ToDos.self, from: data)
+                        onSeccess(items)
+                    } else {
+                        // handle error
+                        let err = try JSONDecoder().decode(APIError.self, from: data)
+                    }
+                } catch {
+                    debugPrint(error.localizedDescription)
+                }
             }
-            
         }
         task.resume()
     }
-
+    
     func addToDo(todo: ToDo) {
-
+        
     }
 }
