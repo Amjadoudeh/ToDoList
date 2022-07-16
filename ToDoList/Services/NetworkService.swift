efad1f7f68c5badb0df4d098f332b2570fd37627
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-16 16:57:23 +0200

    Create NetworkService

---
+++ ToDoList/Services/NetworkService.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct NetworkService {
+    static let shared = NetworkService()
+
+    let URL_BASE = "http://localhost:3003"
+    let URL_ADD_TODO = "/add"
+
+    let session = URLSession(configuration: .default)
+
+    func getToDo() {
+        let url = URL(string: "\(URL_BASE)")!
+
+        let task = session.dataTask(with: url) { (data, _, _) in
+            debugPrint(data)
+        }
+        task.resume()
+    }
+
+    func addToDo(todo: ToDo) {
+
+    }
+}
